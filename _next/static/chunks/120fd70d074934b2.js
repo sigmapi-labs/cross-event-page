bec6ed9425f55ea5b004c84e3e8b5192df870cf0
배정수 <97235836+Devartrio@users.noreply.github.com>
2026-09-03 17:19:51 +0900

    Deploy landing with hero video and 1800+ results

---
+++ _next/static/chunks/120fd70d074934b2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/7a046a6a5ebf5710.js",
+  "static/chunks/turbopack-95e4c8a97d1360a1.js"
+])
---
+++ _next/static/chunks/1d585787c35a8e26.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/7a046a6a5ebf5710.js",
+  "static/chunks/turbopack-24f4a0269dd8ce3a.js"
+])
